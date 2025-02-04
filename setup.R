99ad5635d81a5203715332cb98971dd7842b3bfa
Dhrumil Mehta <dhrumil.mehta@columbia.edu>
2025-02-04 04:29:29 -0500

    Add setup scripts

--- setup.R
+++ setup.R
@@ -2,13 +2,13 @@
 
 # A function that installs packages
 installPackage <- function(pkg) {
-	install.packages(pkg, repos='http://cran.us.r-project.org', verbose = TRUE)
+	install.packages(pkg, repos='http://cran.us.r-project.org', verbose = TRUE, dependencies = TRUE)
 }
 
 # A list of packages to install, including tidyverse
 # (this includes ggplot2, dplyr, and several others...)
 # https://www.tidyverse.org/packages/
-libs <- c("tidyverse", "extrafont", "Cairo", "devtools","gridExtra")
+libs <- c("tidyverse", "extrafont", "Cairo", "devtools","gridExtra", "sf", "tidycensus")
 
 # Apply the function above to the list of packages
 lapply(libs, installPackage)
---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install -y r-base
+R --no-save --quiet < your_script.R
+pip3 install -r requirements.txt
+
